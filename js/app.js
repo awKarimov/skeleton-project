709e238cc023dbc4b008027f36aedf443b943166
karimov <telegramemail09@gmail.com>
2025-10-04 19:04:38 +0500

    finish awful

--- js/app.js
+++ js/app.js
@@ -3,11 +3,14 @@ import {
   elParent,
   elLoader,
   errorMessage,
+  elModal,
+  elCloseModalBtn,
+  elOpenModalBtn,
 } from "./html-elements.js";
 
 function init() {
   elLoader.classList.remove("hidden");
-  fetch("https://json-api.uz/api/project/fn43/cars")
+  fetch("https://json-api.uz/api/project/fn44/cars")
     .then((res) => {
       return res.json();
     })
@@ -35,6 +38,7 @@ function ui(cars) {
     const elMaxSpeed = clone.querySelector("#maxSpeed");
     const elHorsePower = clone.querySelector("#horesePower");
     const elColorName = clone.querySelector("#colorName");
+    const elDeleteBtn = clone.querySelector(".delete-btn");
 
     elTitle.innerHTML = `<strong>${element.name}</strong>`;
     elDesc.innerHTML = `<strong>${element.description}</strong>`;
@@ -43,6 +47,95 @@ function ui(cars) {
     elMaxSpeed.innerHTML = `<strong>Max Tezligi:</strong> ${element.maxSpeed}`;
     elHorsePower.innerHTML = `<strong>Ot Kuchi:</strong> ${element.horsepower}`;
     elColorName.innerHTML = `<strong>Rangi:</strong> ${element.colorName}`;
+    elDeleteBtn.id = element.id;
     elParent.append(clone);
   });
 }
+
+function deleteCar(id) {
+  fetch(`https://json-api.uz/api/project/fn44/cars/${id}`, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      return res.text();
+    })
+    .then((res) => {
+      alert(res);
+      elParent.innerHTML = "";
+      init();
+    })
+    .catch(() => {
+      errorMessage.classList.remove("hidden");
+    })
+    .finally(() => {
+      elLoader.classList.add("hidden");
+    });
+}
+
+function addCar(car) {
+  elLoader.classList.remove("hidden");
+  fetch("https://json-api.uz/api/project/fn44/cars", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(car),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      init();
+    })
+    .catch(() => {
+      errorMessage.classList.remove("hidden");
+    })
+    .finally(() => {
+      elLoader.classList.add("hidden");
+    });
+}
+
+// CRUD
+
+elParent.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-btn")) {
+    if (confirm("Rostan ham o`chirasizmi?")) {
+      e.target.innerText = "Loading ...";
+      deleteCar(e.target.id);
+    }
+  }
+});
+
+const elAddCarForm = document.getElementById("addCarForm");
+
+elAddCarForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(elAddCarForm);
+
+  const newCar = {
+    name: formData.get("name"),
+    description: formData.get("description"),
+    year: +formData.get("year"),
+    acceleration: formData.get("acceleration"),
+    maxSpeed: +formData.get("maxSpeed"),
+    horsepower: +formData.get("horsepower"),
+    colorName: formData.get("colorName"),
+  };
+
+  addCar(newCar);
+  elAddCarForm.reset();
+});
+
+elOpenModalBtn.addEventListener("click", () => {
+  elModal.classList.remove("hidden");
+  elModal.classList.add("flex");
+});
+
+elCloseModalBtn.addEventListener("click", () => {
+  elModal.classList.add("hidden");
+  elModal.classList.remove("flex");
+});
+
+elAddCarForm.addEventListener("submit", () => {
+  elModal.classList.add("hidden");
+  elModal.classList.remove("flex");
+});
--- js/html-elements.js
+++ js/html-elements.js
@@ -2,5 +2,20 @@ const elCardTemp = document.getElementById("cardTemp");
 const elParent = document.getElementById("parent");
 const elLoader = document.getElementById("loader");
 const errorMessage = document.getElementById("error-message");
+const elToastContainer = document.getElementById("toastConatiner");
+const elTemplateContainer = document.getElementById("toastTemplate");
+const elModal = document.getElementById("addCarModal");
+const elOpenModalBtn = document.getElementById("openModalBtn");
+const elCloseModalBtn = document.getElementById("closeModalBtn");
 
-export { elCardTemp, elParent, elLoader, errorMessage };
+export {
+  elCardTemp,
+  elParent,
+  elLoader,
+  errorMessage,
+  elToastContainer,
+  elTemplateContainer,
+  elModal,
+  elOpenModalBtn,
+  elCloseModalBtn,
+};
